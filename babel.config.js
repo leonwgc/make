6b378c6d5602449be1226de4cd11872005436b77
leonwgc <giantfish@126.com>
2021-05-30 17:05:59 +0800

    feat: use vited

--- babel.config.js
+++ babel.config.js
@@ -1,24 +1,11 @@
-module.exports = (api) => {
-  api.cache.using(() => process.env.NODE_ENV);
-
-  const rt = {
-    presets: [
-      [
-        '@babel/preset-env',
-        {
-          targets: {
-            browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9'],
-          },
-        },
-      ],
-      ['@babel/preset-react'],
-    ],
-    plugins: [
-      ['@babel/plugin-proposal-decorators', { legacy: true }],
-      ['@babel/plugin-proposal-class-properties', { loose: false }],
-      ['@babel/plugin-transform-runtime'],
-    ].filter(Boolean),
-  };
-
-  return rt;
+module.exports = {
+  presets: [
+    require.resolve('@babel/preset-env'),
+    require.resolve('@babel/preset-react'),
+    require.resolve('@babel/preset-typescript'),
+  ],
+  plugins: [
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: false }],
+  ],
 };
